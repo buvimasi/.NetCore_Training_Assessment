cac39110cd5b1777d0ffe84dc2bdba86db9f9cad
Buvaneshwari Masilamani <BuvaneshwariM@softura.com>
2022-09-08 09:27:06 +0530

    Task1

---
+++ Training.Domain/Data/EmployeeDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.Domain.Entities;
+
+namespace Training.Domain.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Employees has one to many relationship with Skillmap
+            modelBuilder.Entity<SkillMap>()
+                  .HasOne(bc => bc.Employees)
+                  .WithMany(b => b.SkillMaps)
+                  .HasForeignKey(bc => bc.EmployeeId);
+            //Skills has one to many relationship with Skillmap
+            modelBuilder.Entity<SkillMap>()
+                   .HasOne(bc => bc.Skills)
+                   .WithMany(b => b.SkillMaps)
+                   .HasForeignKey(bc => bc.SkillId);
+            //Employees has one to many relationship with SoftLock
+            modelBuilder.Entity<SoftLock>()
+               .HasOne(bc => bc.Employees)
+               .WithMany(b => b.SoftLocks)
+               .HasForeignKey(bc => bc.EmployeeId);
+
+
+        }
+
+        public DbSet<Employees> Employees { get; set; }
+        public DbSet<Skills> Skills { get; set; }
+        public DbSet<SkillMap> SkillMaps { get;set; }
+        public DbSet<SoftLock> SoftLocks { get; set; }
+    }
+}
+
